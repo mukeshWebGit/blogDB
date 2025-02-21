930df9f4d565e786dbb7f0b9dad3e230544471b3
Mukesh Kumar <mmuskan,940@gmail.com>
2025-02-21 22:26:06 +0530

    login function add

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,10 +1,15 @@
 import User from '../model/user.js';   
-import bcrupt from 'bcrypt'; 
+import Jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'; 
+import dotenv from 'dotenv';
+import Token from '../model/token.js';
+
+dotenv.config();
 export const SignupUser = async (request, response) => { 
   
   try{
-    const salt = await bcrupt.genSalt();
-    const hashPassword = await bcrupt.hash(request.body.password, 10)
+    //const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(request.body.password, 10)
     const user = {user: request.body.user, name:request.body.name, password:hashPassword};
     const newUser = new User (user);
     newUser.save();
@@ -16,4 +21,24 @@ export const SignupUser = async (request, response) => {
   }
 
 }
- 
+export const LoginUser = async (request, response) => { 
+  let user = await User.findOne({userName : request.body.userName})
+  if (!user){
+    return response.status(400).json({msg:'Username does not match'})
+  }
+  try{
+    let match = await bcrypt.compare(request.body.password, user.password);
+    if(match){ 
+      const accessToken = Jwt.sign(user.toJSON(), process.env.ACCESS_SECRET_KEY, {expiresIn: '15m'});
+      const refressToken = Jwt.sign(user.toJSON(), process.env.REFRESS_SECRET_KEY);
+      const newToken = new Token({token: refressToken});
+      await newToken.save();
+      return response.status(200).json({accessToken:accessToken, refressToken:refressToken, name:user.name, userName:user.userName})
+  
+    }else{
+      return response.status(400).json({msg:'Password does not match'})
+    }
+  }catch(error){
+     return response.status(500).json({msg:'Error while login user'}) 
+  }
+  }
--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import {SignupUser} from '../controller/user-controller.js';
+import {SignupUser, LoginUser} from '../controller/user-controller.js';
 
 const Router = express.Router();
 
 Router.post('/signup', SignupUser);
-//Router.post('/login', LoginUser);
+Router.post('/login', LoginUser);
 
 export default Router;
